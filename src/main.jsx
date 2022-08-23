f257b8c335fb1104d129674437bbc777124044d7
MiguelD-PC <mikdominguez@gmail.com>
2022-08-23 18:18:40 +0100

    update

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,9 @@
+
+import "./index.css";
+import { OrthoApp } from "./orthoApp";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
-import { OrthoApp } from "./orthoApp";
-import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <BrowserRouter>
